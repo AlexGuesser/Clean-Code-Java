19eac88e14e2e2fd1fada13a35f204b9b08f4370
Axonify-Alex-Guesser <140539263+Axonify-Alex-Guesser@users.noreply.github.com>
2025-02-24 18:18:25 -0300

    Adjusts

---
+++ backend/query/src/main/java/dev/alexguesser/query/infra/configuration/BeansConfig.java
@@ -0,0 +1,16 @@
+package dev.alexguesser.query.infra.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class BeansConfig {
+
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.create();
+    }
+
+}
